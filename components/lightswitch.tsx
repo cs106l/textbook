126037e511cebe7eb911b491f61ccab5028a092a
JTRB <jacobrobertsbaca@gmail.com>
2024-12-19 23:52:03 -0700

    fix: lightswitch flicker

--- components/lightswitch.tsx
+++ components/lightswitch.tsx
@@ -1,20 +1,30 @@
 "use client";
 
-import { IconButton, SvgIcon, useColorScheme } from "@mui/material";
+import { IconButton, SvgIcon, useColorScheme, useTheme } from "@mui/material";
 
 import LightIcon from "@heroicons/react/24/solid/MoonIcon";
 import DarkIcon from "@heroicons/react/24/solid/SunIcon";
 
 export default function Lightswitch() {
   const { mode, setMode } = useColorScheme();
+  const theme = useTheme();
 
   return (
     <IconButton onClick={() => setMode(mode === "dark" ? "light" : "dark")}>
       <SvgIcon
-        sx={{ color: "var(--palette-common-onBackground)" }}
+        sx={{
+          color: "var(--palette-common-onBackground)",
+          ...theme.applyStyles("light", {
+            "& .dark": { display: "none" },
+          }),
+          ...theme.applyStyles("dark", {
+            "& .light": { display: "none" },
+          }),
+        }}
         fontSize="small"
       >
-        {mode === "dark" ? <DarkIcon /> : <LightIcon />}
+        <DarkIcon className="dark" />
+        <LightIcon className="light" />
       </SvgIcon>
     </IconButton>
   );
